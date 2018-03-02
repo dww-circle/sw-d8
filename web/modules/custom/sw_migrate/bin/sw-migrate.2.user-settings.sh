2e3709c9c5bef6ed6deb02db683b7da75d6e419e
Derek Wright <git@dwwright.net>
2018-03-02 15:52:14 -0800

    Split user + role settings (phase 2) from actual user migration (now phase 4).

--- web/modules/custom/sw_migrate/bin/sw-migrate.2.user-settings.sh
+++ web/modules/custom/sw_migrate/bin/sw-migrate.2.user-settings.sh
@@ -6,9 +6,3 @@ drush mim upgrade_d6_user_mail
 
 # Roles
 drush mim upgrade_d6_user_role
-
-# Actual users
-drush mim upgrade_d6_user
-
-# User contact settings
-drush mim upgrade_d6_user_contact_settings
---
+++ web/modules/custom/sw_migrate/bin/sw-migrate.4.users.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Actual users
+drush mim upgrade_d6_user
+
+# User contact settings
+drush mim upgrade_d6_user_contact_settings
