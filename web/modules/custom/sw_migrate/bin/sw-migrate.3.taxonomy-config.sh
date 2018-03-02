ffae46f9e75d47ca98fa9a9fcf2f2cf9cad3ded3
Derek Wright <git@dwwright.net>
2018-03-02 15:54:55 -0800

    Split taxonomy + vocab settings (phase 3) from actual term migration (now phase 4).

--- web/modules/custom/sw_migrate/bin/sw-migrate.3.taxonomy-config.sh
+++ web/modules/custom/sw_migrate/bin/sw-migrate.3.taxonomy-config.sh
@@ -8,6 +8,3 @@ drush mim upgrade_d6_vocabulary_field
 drush mim upgrade_d6_vocabulary_field_instance
 drush mim upgrade_d6_vocabulary_entity_display
 drush mim upgrade_d6_vocabulary_entity_form_display
-
-# Terms
-drush mim upgrade_d6_taxonomy_term --feedback=500
---
+++ web/modules/custom/sw_migrate/bin/sw-migrate.4.taxonomy-terms.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# Terms
+drush mim upgrade_d6_taxonomy_term --feedback=500
