8784f036a0825f33fa143834dbe4e7904bd944f7
Derek Wright <git@dwwright.net>
2018-03-03 14:06:54 -0800

    Fix node revision migration script to use correct migration names.

--- web/modules/custom/sw_migrate/bin/sw-migrate.6.node-revisions.sh
+++ web/modules/custom/sw_migrate/bin/sw-migrate.6.node-revisions.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # Actual node revisions.
-drush mim upgrade_d6_node_revision:page
-drush mim upgrade_d6_node_revision:blog_couldnt_make_it_up
-drush mim upgrade_d6_node_revision:blog_critical_reading --feedback=100
-# drush mim upgrade_d6_node_revision:image    // Ugh, need to be converted to media entities
-drush mim upgrade_d6_node_revision:insert_box --feedback=100
-drush mim upgrade_d6_node_revision:person --feedback=100
-drush mim upgrade_d6_node_revision:story --feedback=100
+drush mim upgrade_d6_node_revision_page
+drush mim upgrade_d6_node_revision_blog_couldnt_make_it_up
+drush mim upgrade_d6_node_revision_blog_critical_reading --feedback=100
+# drush mim upgrade_d6_node_revision_image    // Ugh, need to be converted to media entities
+drush mim upgrade_d6_node_revision_insert_box --feedback=100
+drush mim upgrade_d6_node_revision_person --feedback=100
+drush mim upgrade_d6_node_revision_story --feedback=100
