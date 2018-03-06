618a363a7f87da93d3d4b4120fa173311139a976
Derek Wright <git@dwwright.net>
2018-03-05 20:54:10 -0800

    Remove sytem config migrations that are irrelevant (we're already using the defaults, etc).

--- web/modules/custom/sw_migrate/bin/sw-migrate.1.config.sh
+++ web/modules/custom/sw_migrate/bin/sw-migrate.1.config.sh
@@ -4,7 +4,3 @@
 
 # Settings / configuration
 drush mim upgrade_d6_filter_format
-drush mim upgrade_d6_system_cron
-drush mim upgrade_d6_system_file
-drush mim upgrade_system_image
-drush mim upgrade_system_logging
