dbb5a1bff96df6f5badc9500d18155aadcbba279
Derek Wright <git@dwwright.net>
2018-03-05 20:29:24 -0800

    Migrate system.date config and remove migration code.

--- web/modules/custom/sw_migrate/bin/sw-migrate.1.config.sh
+++ web/modules/custom/sw_migrate/bin/sw-migrate.1.config.sh
@@ -5,7 +5,6 @@
 # Settings / configuration
 drush mim upgrade_d6_filter_format
 drush mim upgrade_d6_system_cron
-drush mim upgrade_d6_system_date
 drush mim upgrade_d6_system_file
 drush mim upgrade_system_image
 drush mim upgrade_system_logging
