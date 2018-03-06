71943eb186303f9463908c10b6142bb42abc9646
Derek Wright <git@dwwright.net>
2018-03-05 16:48:48 -0800

    Use the most recent DB export for starting migrations.

--- web/modules/custom/sw_migrate/bin/sw-migrate.0.reset-db.sh
+++ web/modules/custom/sw_migrate/bin/sw-migrate.0.reset-db.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 drush -y sql-drop
-gzcat /Applications/MAMP/htdocs/sw-d8/dumps/2018-02-18.dev-repaired.2.sql.gz | `drush sql-connect`
+gzcat /Applications/MAMP/htdocs/sw-d8/dumps/sw-d8_dev_2018-03-05T19-44-22_UTC_database.sql.gz | `drush sql-connect`
 drush -y updb
 drush -y cim
+drush -y en sw_migrate
 drush cr
