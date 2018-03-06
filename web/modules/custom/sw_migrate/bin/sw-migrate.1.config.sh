55f6beac50d32daaa430a49c4643e292d4974366
Derek Wright <git@dwwright.net>
2018-03-05 19:55:22 -0800

    Migrate search-related config and remove migration code.

--- web/modules/custom/sw_migrate/bin/sw-migrate.1.config.sh
+++ web/modules/custom/sw_migrate/bin/sw-migrate.1.config.sh
@@ -4,12 +4,10 @@
 
 # Settings / configuration
 drush mim upgrade_d6_filter_format
-drush mim upgrade_d6_search_settings
 drush mim upgrade_d6_system_cron
 drush mim upgrade_d6_system_date
 drush mim upgrade_d6_system_file
 drush mim upgrade_d6_system_performance
-drush mim upgrade_search_page
 drush mim upgrade_system_image
 drush mim upgrade_system_logging
 drush mim upgrade_system_site
