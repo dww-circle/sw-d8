3f719d27f4f171579dae7ea41cccc070786b0102
Derek Wright <git@dwwright.net>
2018-03-08 02:12:44 -0800

    Improved 5.nodes migration driver script to log output, track times, etc.

--- web/modules/custom/sw_migrate/bin/sw-migrate.5.nodes.sh
+++ web/modules/custom/sw_migrate/bin/sw-migrate.5.nodes.sh
@@ -1,17 +1,27 @@
-#!/bin/sh
+#!/bin/zsh
 
 # Actual nodes
-drush mim upgrade_d6_node_page
-drush mim upgrade_d6_node_blog_couldnt_make_it_up
-drush mim upgrade_d6_node_blog_critical_reading
+
+#for bundle (d6_node_page d6_node_blog_couldnt_make_it_up d6_node_blog_critical_reading d6_node_insert_box d6_node_person d6_node_story ) {
+#for bundle (d6_node_page d6_node_blog_couldnt_make_it_up) {
+for bundle (d6_node_blog_critical_reading d6_node_insert_box d6_node_person d6_node_story ) {
+  echo "Working on $bundle";
+  rm -f $bundle.out $bundle.err
+  time drush mim upgrade_$bundle > $bundle.out 2> $bundle.err
+}
+
+#drush mim upgrade_d6_node_page
+#drush mim upgrade_d6_node_blog_couldnt_make_it_up
+#drush mim upgrade_d6_node_blog_critical_reading
+#drush mim upgrade_d6_node_insert_box
+#drush mim upgrade_d6_node_person
+
 #drush mim upgrade_d6_node_image    # Ugh, need to be converted to media entities
-drush mim upgrade_d6_node_insert_box
-drush mim upgrade_d6_node_person
 
 # Needs all sorts of help:
 # - Remapping from image nodes to image media entities
 # - Convert DME tags
 # - Set main image field
-# - Populate address fields (from taxonomy, whoops)
-# - Convert contributors from taxonomy to person nodes (?)
-#drush mim upgrade_d6_node_story --feedback=100
+# - rewrite hyphens to <hr>
+# ?
+#drush mim upgrade_d6_node_story
