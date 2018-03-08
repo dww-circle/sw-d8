c1b4f2c212a6480ef24bc997f053b4eb6774a186
Derek Wright <git@dwwright.net>
2018-03-08 02:21:48 -0800

    Split term-node and term-node-revision drivers into separate steps.

---
+++ web/modules/custom/sw_migrate/bin/sw-migrate.12.term-node-revisions.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Mapping terms to node revisions
+drush mim upgrade_d6_term_node_revision:1
+drush mim upgrade_d6_term_node_revision:2
+drush mim upgrade_d6_term_node_revision:4
+drush mim upgrade_d6_term_node_revision:5
+drush mim upgrade_d6_term_node_revision:6
+drush mim upgrade_d6_term_node_revision:7
+drush mim upgrade_d6_term_node_revision:8
+drush mim upgrade_d6_term_node_revision:9
+drush mim upgrade_d6_term_node_revision:10
+drush mim upgrade_d6_term_node_revision:18
+drush mim upgrade_d6_term_node_revision:12
+drush mim upgrade_d6_term_node_revision:13
+drush mim upgrade_d6_term_node_revision:14
+drush mim upgrade_d6_term_node_revision:15
+drush mim upgrade_d6_term_node_revision:16
+drush mim upgrade_d6_term_node_revision:17
