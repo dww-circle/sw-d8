4315ad7ea341849519569c02bb0921a860e6b35c
Derek Wright <git@dwwright.net>
2018-03-08 02:28:21 -0800

    Enhance migration driver scripts for term-node and term-node-revision to log output + timing.

--- web/modules/custom/sw_migrate/bin/sw-migrate.7.term-node.sh
+++ web/modules/custom/sw_migrate/bin/sw-migrate.7.term-node.sh
@@ -1,19 +1,11 @@
-#!/bin/sh
+#!/bin/zsh
 
-# Mapping terms to nodes
-drush mim upgrade_d6_term_node:1
-drush mim upgrade_d6_term_node:2
-drush mim upgrade_d6_term_node:4
-drush mim upgrade_d6_term_node:5
-drush mim upgrade_d6_term_node:6
-drush mim upgrade_d6_term_node:7
-drush mim upgrade_d6_term_node:8
-drush mim upgrade_d6_term_node:9
-drush mim upgrade_d6_term_node:10
-drush mim upgrade_d6_term_node:18
-drush mim upgrade_d6_term_node:12
-drush mim upgrade_d6_term_node:13
-drush mim upgrade_d6_term_node:14
-drush mim upgrade_d6_term_node:15
-drush mim upgrade_d6_term_node:16
-drush mim upgrade_d6_term_node:17
+# Map terms to nodes
+
+# vids 3 and 11 are gone, so we have to enumerate these.
+for i (1 2 4 5 6 7 8 9 10 12 13 14 15 16 17 18) {
+  echo "Working on vocabulary ID $i";
+  file="term_node_vid_$i";
+  rm -f $file.out $file.err;
+  time drush mim "upgrade_d6_term_node_$i" > $file.out 2> $file.err;
+}
