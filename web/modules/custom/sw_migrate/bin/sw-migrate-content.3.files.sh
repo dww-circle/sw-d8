a45b2345a45fb05f3091bbd2842d713b81112246
Derek Wright <git@dwwright.net>
2018-03-08 11:52:51 -0800

    File migration: fix driver script to log output + times, drop the --feedback.

--- web/modules/custom/sw_migrate/bin/sw-migrate-content.3.files.sh
+++ web/modules/custom/sw_migrate/bin/sw-migrate-content.3.files.sh
@@ -1,6 +1,10 @@
-#!/bin/sh
+#!/bin/zsh
 
 # Files
-# Really fucked up...
-drush mim upgrade_d6_file --feedback=200
-drush mim upgrade_d6_upload
+rm -f d6_file.out d6_file.err
+time drush mim upgrade_d6_file > d6_file.out 2> d6_file.err
+
+# Uploads (e.g. files attached to nodes)
+rm -f d6_upload.out d6_upload.err
+time drush mim upgrade_d6_upload > d6_upload.out 2> d6_upload.err
+
