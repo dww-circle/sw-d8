57db3cf51b6f866d549d21293abe338e9d2719c7
Derek Wright <git@dwwright.net>
2018-03-08 05:16:53 -0800

    Further reorg of migration driver scripts:
    - Since config is done and shipped, moving to 'sw-migrate-content' namespace
    - Renumbering content phases from 1 to make things a bit easier
    - We'll need to migrate files quite early, before any nodes.
    - Split image media from the rest of nodes:
    -- It's still TODO and will need lots of help
    -- The story migration will depend on it

---
+++ web/modules/custom/sw_migrate/bin/sw-migrate-content.4.image-media.sh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+
+# Convert D6 image nodes into D8 image media entities
+
+# TODO!
+#time drush mim upgrade_d6_node_image > d6_node_image.out 2> d6_node_image.err
---
+++ web/modules/custom/sw_migrate/bin/sw-migrate-content.5.nodes.sh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+
+# Actual nodes
+
+# 'story' still needs all sorts of help:
+# - Convert DME and move tags
+# - Set main image field
+# - rewrite hyphens to <hr>
+# ?
+for bundle (d6_node_page d6_node_person d6_node_insert_box d6_node_blog_couldnt_make_it_up d6_node_blog_critical_reading d6_node_story) {
+  echo "Working on $bundle";
+  rm -f $bundle.out $bundle.err;
+  time drush mim upgrade_$bundle > $bundle.out 2> $bundle.err;
+}
