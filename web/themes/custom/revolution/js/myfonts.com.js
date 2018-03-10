f4fa3d30de0eb7fa7715555eaecbb871cc2a178f
Zakiya <zakiya@chapterthree.com>
2018-03-09 19:14:12 -0800

    Theme - font updates

---
+++ web/themes/custom/revolution/js/myfonts.com.js
@@ -0,0 +1,9 @@
+// Intializes font. See https://www.myfonts.com/fonts/fontfont/ff-din/webfont_preview.html Good for 30 days.
+(function() {
+  var trial = document.createElement('script');
+  trial.type = 'text/javascript';
+  trial.async = true;
+  trial.src = 'https://easy.myfonts.net/v2/js?sid=161376(font-family=FF+DIN+Pro+Medium)&sid=161379(font-family=FF+DIN+Pro+Regular)&sid=161380(font-family=FF+DIN+Pro+Bold)&sid=290199(font-family=FF+DIN+Pro+Extra+Light)&key=SlmOcVxePJ';
+  var head = document.getElementsByTagName("head")[0];
+  head.appendChild(trial);
+})();
