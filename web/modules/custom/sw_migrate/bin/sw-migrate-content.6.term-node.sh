6acde71e05708df742c20ea404e8096723140bc3
Derek Wright <git@dwwright.net>
2018-03-16 01:00:18 -0700

    Migrate: generalize the taxonomy handling for d6_node_story to add issue # and series terms.

--- web/modules/custom/sw_migrate/bin/sw-migrate-content.6.term-node.sh
+++ web/modules/custom/sw_migrate/bin/sw-migrate-content.6.term-node.sh
@@ -2,10 +2,10 @@
 
 # Map D6 terms to nodes.
 echo "Starting node term migration.";
-# vids 1, 2 and 10 are handled directly during d6_node_story.
+# vids 1, 2, 5, 10 and 12 are handled directly during d6_node_story.
 # vids 2 and 13 are handled directly during d6_node_insert_box.
 # vids 3 and 11 are gone, so we have to enumerate the rest.
-for i (4 5 6 7 8 9 12) {
+for i (4 6 7 8 9) {
   echo "Working on vocabulary ID $i";
   file="term_node_vid_$i";
   rm -f $file.out
