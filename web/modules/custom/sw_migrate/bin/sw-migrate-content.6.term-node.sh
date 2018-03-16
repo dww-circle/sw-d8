12283df1accffecd4b9d1b88f4fcfc313a0e46c1
Derek Wright <git@dwwright.net>
2018-03-15 23:16:09 -0700

    [SW #982] Migrate: Preserve the 2.0 story type as a custom label in some cases.
    - While preparing our row for d6_node_story, query for the 2.0 story type term.
    - If there's a 2.0 story type, and the 3.0 story type would be empty, and
      either there are no authors or the 'Include names in the story label'
      checkbox was off, stash the 2.0 story type name into the custom 3.0
      story label field and set the 3.0 story type to 'custom'
    - Instead of doing it in a whole separate migration, we can save the
      right story type term directly (since we already had to compute it).

--- web/modules/custom/sw_migrate/bin/sw-migrate-content.6.term-node.sh
+++ web/modules/custom/sw_migrate/bin/sw-migrate-content.6.term-node.sh
@@ -2,8 +2,9 @@
 
 # Map D6 terms to nodes.
 echo "Starting node term migration.";
-# vids 3 and 11 are gone, so we have to enumerate these.
-for i (1 2 4 5 6 7 8 9 10 12 13) {
+# vid 10 is handled directly during d6_node_story.
+# vids 3 and 11 are gone, so we have to enumerate the rest.
+for i (1 2 4 5 6 7 8 9 12 13) {
   echo "Working on vocabulary ID $i";
   file="term_node_vid_$i";
   rm -f $file.out
