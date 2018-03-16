66fb46e45079808690144a6134e09cf9c866385a
Derek Wright <git@dwwright.net>
2018-03-15 23:26:06 -0700

    Migrate topic and section terms directly during story migration.

--- web/modules/custom/sw_migrate/bin/sw-migrate-content.6.term-node.sh
+++ web/modules/custom/sw_migrate/bin/sw-migrate-content.6.term-node.sh
@@ -2,9 +2,9 @@
 
 # Map D6 terms to nodes.
 echo "Starting node term migration.";
-# vid 10 is handled directly during d6_node_story.
+# vids 1, 2 and 10 are handled directly during d6_node_story.
 # vids 3 and 11 are gone, so we have to enumerate the rest.
-for i (1 2 4 5 6 7 8 9 12 13) {
+for i (4 5 6 7 8 9 12 13) {
   echo "Working on vocabulary ID $i";
   file="term_node_vid_$i";
   rm -f $file.out
