1643317e3a838ae7a8b879e3bc7d8012793b5a26
Derek Wright <git@dwwright.net>
2018-03-15 23:53:47 -0700

    Migrate topic and insert type directly during d6_node_insert_box.

--- web/modules/custom/sw_migrate/bin/sw-migrate-content.6.term-node.sh
+++ web/modules/custom/sw_migrate/bin/sw-migrate-content.6.term-node.sh
@@ -3,8 +3,9 @@
 # Map D6 terms to nodes.
 echo "Starting node term migration.";
 # vids 1, 2 and 10 are handled directly during d6_node_story.
+# vids 2 and 13 are handled directly during d6_node_insert_box.
 # vids 3 and 11 are gone, so we have to enumerate the rest.
-for i (4 5 6 7 8 9 12 13) {
+for i (4 5 6 7 8 9 12) {
   echo "Working on vocabulary ID $i";
   file="term_node_vid_$i";
   rm -f $file.out
