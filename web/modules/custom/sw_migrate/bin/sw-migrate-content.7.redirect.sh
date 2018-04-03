385b232b6435fd4935232c2d3252006900fbd2a9
Derek Wright <git@dwwright.net>
2018-04-03 12:30:47 -0700

    [SW #986]: Add CSV migration for redirects (initially for legacy /department links).

--- web/modules/custom/sw_migrate/bin/sw-migrate-content.7.redirect.sh
+++ web/modules/custom/sw_migrate/bin/sw-migrate-content.7.redirect.sh
@@ -1,4 +1,5 @@
 #!/bin/zsh
 
-rm -f 7.redirect.out
-/usr/bin/time drush mim upgrade_d6_path_redirect > 7.redirect.out 2>&1
+rm -f 7.redirect_d6.out 7.redirect_csv.out 
+/usr/bin/time drush mim upgrade_d6_path_redirect > 7.redirect_d6.out 2>&1
+/usr/bin/time drush mim csv_redirect > 7.redirect_csv.out 2>&1
