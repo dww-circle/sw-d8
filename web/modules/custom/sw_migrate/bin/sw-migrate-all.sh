af3a061c0223c6546ee7eb595d2da33e2dac1d06
Derek Wright <git@dwwright.net>
2018-04-04 15:41:46 -0700

    Migrate: Handle all image taxonomy term associations directly in the initial media migration.

--- web/modules/custom/sw_migrate/bin/sw-migrate-all.sh
+++ web/modules/custom/sw_migrate/bin/sw-migrate-all.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-for phase ('1.users' '2.files' '3.taxonomy-terms' '4.image-media' '5.nodes' '6.term-media' '6.term-node' '7.url_alias' '7.redirect' '7.node-upload') {
+for phase ('1.users' '2.files' '3.taxonomy-terms' '4.image-media' '5.nodes' '6.term-node' '7.url_alias' '7.redirect' '7.node-upload') {
   rm -f $phase.out;
   script="./sw-migrate-content.$phase.sh";
   echo "Running $script";
