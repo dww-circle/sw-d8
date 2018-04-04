9ed8c180921d172f0897c92bf682f35272beb710
Derek Wright <git@dwwright.net>
2018-04-04 14:11:04 -0700

    Migrate: Update the source DB and files for clean migration runs.

--- web/modules/custom/sw_migrate/bin/sw-migrate-content.0.reset-db.sh
+++ web/modules/custom/sw_migrate/bin/sw-migrate-content.0.reset-db.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 drush -y sql-drop
-gzcat /Applications/MAMP/htdocs/sw-d8/dumps/2018-03-07.migrate.config-all.clean.sql.gz | `drush sql-connect`
+gzcat /Applications/MAMP/htdocs/sw-d8-dumps/2018-04-04.migrate-clean-start.sql.gz | `drush sql-connect`
 drush -y updb
 drush -y cim
 drush -y en sw_migrate
--- web/modules/custom/sw_migrate/bin/sw-migrate-content.0.reset-files.sh
+++ web/modules/custom/sw_migrate/bin/sw-migrate-content.0.reset-files.sh
@@ -2,6 +2,6 @@
 
 rm -rf /Applications/MAMP/htdocs/sw-d8/web/sites/default/files
 cd /Applications/MAMP/htdocs/sw-d8/web/sites/default
-tar -zxvf /Applications/MAMP/htdocs/sw-d8/dumps/sw-d8_dev_2018-03-05T19-46-55_UTC_files.tar.gz
+tar -zxvf /Applications/MAMP/htdocs/sw-d8-dumps/sw-d8_dev_2018-03-05T19-46-55_UTC_files.tar.gz
 mv files_dev files
 find files -type d | xargs chmod 1777
