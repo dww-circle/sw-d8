3c9de777dd964564333c41fd80a5056592eb062d
Derek Wright <git@dwwright.net>
2018-04-13 07:55:09 -0700

    [SW #980]: Add a custom filter to replace <sw-series> with the automatic insert box.

---
+++ web/modules/custom/sw/src/Plugin/Filter/SWFilterSeries.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Drupal\sw\Plugin\Filter;
+
+use Drupal\Core\Routing;
+use Drupal\filter\FilterProcessResult;
+use Drupal\filter\Plugin\FilterBase;
+use Drupal\node\NodeInterface;
+
+/**
+ * Provides a filter to replace <sw-series> tags with the magic series insert box.
+ *
+ * @Filter(
+ *   id = "sw_filter_series",
+ *   title = @Translation("SW series insert box"),
+ *   description = @Translation("Replaces <code>&lt;sw-series&gt;</code> tags with an automagic series insert box."),
+ *   type = Drupal\filter\Plugin\FilterInterface::TYPE_TRANSFORM_IRREVERSIBLE,
+ * )
+ */
+class SWFilterSeries extends FilterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function process($text, $langcode) {
+    $result = new FilterProcessResult($text);
+    if (stristr($text, 'sw-series') !== FALSE) {
+      $series_box = '';
+      $node = \Drupal::routeMatch()->getParameter('node');
+      if (!empty($node) && $node->bundle() == 'story') {
+        $series_box = sw_insert_series_box($node);
+      }
+      $result->setProcessedText(preg_replace('@<sw-series></sw-series>@', $series_box, $text));
+    }
+    return $result;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function tips($long = FALSE) {
+    return $this->t('Use <code>&lt;sw-series&gt;&lt;/sw-series&gt;</code> for an automagic series insert box.');
+  }
+
+}
