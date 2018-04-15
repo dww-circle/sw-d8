9d4ea870e80bd145105f771c3536a15aabc53acc
Derek Wright <git@dwwright.net>
2018-04-15 14:51:51 -0700

    [SW #991]: Convert 'From the archives' block from a view into a custom block:
    - I couldn't get Views to cache this via a 'route' context, no matter what.
    - Entityqueue doesn't have sufficiently powerful Views integration to
      make the behavior we want possible.
    - So, this is now a custom block with accurate cache metadata and all
      the logic implemented directly in PHP array manipulation.
    - @todo: Some of the values in here should probably be config knobs.

---
+++ web/modules/custom/sw/src/Plugin/Block/SWFromTheArchivesBlock.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Drupal\sw\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\node\Entity\Node;
+
+/**
+ * A block for the story footer region for random 'From the archives' stories.
+ *
+ * @Block(
+ *   id = "sw_from_the_archives_block",
+ *   admin_label = @Translation("SW From the archives"),
+ *   category = @Translation("SW"),
+ * )
+ */
+class SWFromTheArchivesBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    // This block must be cached separately for every page/route. Define this
+    // render array here so that if we bail early, the render system knows to
+    // only cache the empty response for the specific route that generated it.
+    $block = [
+      '#cache' => [
+        'contexts' => ['route'],
+      ],
+    ];
+
+    // Load the entityqueue this is based on.
+    $entityqueue = \Drupal::entityManager()->getStorage('entity_subqueue')->load('from_the_archives');
+    if (empty($entityqueue)) {
+      return $block;
+    }
+
+    // Get the full list of items in the queue.
+    $queue_list = $entityqueue->get('items')->getValue();
+
+    // Restrict ourselves to the top 10 stories in the queue.
+    // @todo: Make this configurable?
+    $active_stories = array_slice($queue_list, 0, 10);
+
+    // Save the nid and delta of these "active" stories.
+    // Preserving the deltas lets us easily sort later.
+    foreach ($active_stories as $delta => $value) {
+      $nids[] = $value['target_id'];
+      $deltas[$value['target_id']] = $delta;
+    }
+
+    // Randomize the nids.
+    shuffle($nids);
+
+    // Harvest the top 5 stories from the random list to display in the block.
+    // @todo: Make this configurable?
+    $block_nids = array_slice($nids, 0, 5);
+
+    // Sort the random stories by the original entityqueue ordering.
+    foreach ($block_nids as $nid) {
+      $ordered_list[$deltas[$nid]] = $nid;
+    }
+    ksort($ordered_list);
+
+    // Load and render these stories as teasers.
+    $items = [];
+    $stories = Node::loadMultiple($ordered_list);
+    $view_builder = \Drupal::entityTypeManager()->getViewBuilder('node');
+    foreach ($stories as $story) {
+      $items[] = $view_builder->view($story, 'teaser');
+    }
+
+    // Our block body is just an item_list of the teaser render arrays.
+    return [
+      '#theme' => 'item_list',
+      '#list_type' => 'ul',
+      '#items' => $items,
+    ] + $block;
+  }
+}
