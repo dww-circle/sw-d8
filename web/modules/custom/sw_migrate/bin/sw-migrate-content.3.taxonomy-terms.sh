eebe98c2323a71b81423b8941410b1c0a34d192d
Derek Wright <git@dwwright.net>
2018-04-19 06:36:50 -0700

    [SW #979]: Migrate: Add a CSV migration for adding new taxonomy terms to SW 3.0:
    Immediate use-case is a 'Custom' term in the 'insert_type' vocabulary.
    This should only use TIDs that don't conflict, using gaps in the 2.0
    data (e.g. terms that were deleted at some point, etc).

--- web/modules/custom/sw_migrate/bin/sw-migrate-content.3.taxonomy-terms.sh
+++ web/modules/custom/sw_migrate/bin/sw-migrate-content.3.taxonomy-terms.sh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 
 # Terms
-rm -f d6_taxonomy_term.out;
+rm -f d6_taxonomy_term.out csv_taxonomy_term.out;
 time drush mim upgrade_d6_taxonomy_term > d6_taxonomy_term.out 2>&1
+time drush mim csv_taxonomy_terms > csv_taxonomy_term.out 2>&1
