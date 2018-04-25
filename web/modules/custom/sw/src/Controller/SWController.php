61459bb92db29f1a40fee7b664393d513ab12008
Derek Wright <git@dwwright.net>
2018-04-25 10:40:42 -0700

    [SW #1021]: Trivial landing page at /topic that shows a list of current submenu items.

--- web/modules/custom/sw/src/Controller/SWController.php
+++ web/modules/custom/sw/src/Controller/SWController.php
@@ -22,4 +22,39 @@ class SWController extends ControllerBase {
     ];
   }
 
+  /**
+   * Builds the /topic landing page.
+   *
+   * Loads any children menu items under /topic and renders them all as a simple
+   * list, ordered by menu item weights.
+   *
+   * @return array
+   *   Render array for the /topic landing page.
+   */
+  public function topicPage() {
+    $build = [];
+    $menu_name = 'main';
+    $menu_link_service = \Drupal::getContainer()->get('plugin.manager.menu.link');
+    $menu_links = $menu_link_service->loadLinksByRoute('sw.topic', [], $menu_name);
+    if (!empty($menu_links)) {
+      $root_menu_item = reset($menu_links);
+      $menu_parameters = new \Drupal\Core\Menu\MenuTreeParameters();
+      $menu_parameters->setMaxDepth(1);
+      $menu_parameters->setRoot($root_menu_item->getPluginId());
+      $menu_parameters->excludeRoot();
+      $menu_tree_service = \Drupal::service('menu.link_tree');
+      $tree = $menu_tree_service->load($menu_name, $menu_parameters);
+      if (!empty($tree)) {
+        $manipulators = [
+          ['callable' => 'menu.default_tree_manipulators:checkNodeAccess'],
+          ['callable' => 'menu.default_tree_manipulators:checkAccess'],
+          ['callable' => 'menu.default_tree_manipulators:generateIndexAndSort'],
+        ];
+        $tree = $menu_tree_service->transform($tree, $manipulators);
+        $build['menu_items'] = $menu_tree_service->build($tree);
+      }
+    }
+    return $build;
+  }
+
 }
