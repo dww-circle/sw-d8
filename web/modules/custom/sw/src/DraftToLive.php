9e5e1163e77310eb25c9c5c4fc758c6d2b08fc50
Derek Wright <git@dwwright.net>
2018-04-26 18:55:28 -0700

    [SW #971]: Draft-to-live: Added logic to create new revisions for multiple archives of the same day.

--- web/modules/custom/sw/src/DraftToLive.php
+++ web/modules/custom/sw/src/DraftToLive.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\sw;
 
+use Drupal\Core\Path\AliasStorage;
+use Drupal\Core\Path\AliasStorageInterface;
 use Drupal\Core\State\StateInterface;
 use Drupal\Core\Url;
 use Drupal\node\Entity\Node;
@@ -68,23 +70,40 @@ class DraftToLive {
     $month = date('m');
     $day = date('d');
     $url_alias = "/archive/front/$year/$month/$day";
-    // @todo: Deal with over-writing an existing record, or use revisions, or something...
+    $alias_storage = \Drupal::service('path.alias_storage');
     try {
       $request = $client->get($url->toString());
       $status = $request->getStatusCode();
-      $node = Node::create(
-        [
-          'type' => 'static_page',
-          'title' => "SocialistWorker raw front page from $year-$month-$day",
-          'field_static_body' => [
-            'value' => $request->getBody()->getContents(),
-          ],
-          'status' => 1,
-          'uid' => $this->requestUID,
-          'path' => $url_alias,
-        ]
-      );
-      $node->save();
+      $raw_html = $request->getBody()->getContents();
+      $alias = $alias_storage->load(['alias' => $url_alias]);
+      // If we already have this front page archived, we want to create a new revision.
+      if (!empty($alias)) {
+        // 'source' will be something like '/node/xxxx', so strip off the first
+        // 6 characters ('/node/') and we have the nid.
+        $nid = substr($alias['source'], 6);
+        $node = Node::load($nid);
+        $node->setNewRevision(TRUE);
+        $node->setRevisionUserId($this->requestUID);
+        $node->revision_log = t('Draft-to-live is updating an existing front page archive.');
+        $node->set('field_static_body', $raw_html);
+        $node->save();
+      }
+      // Otherwise, create a new node.
+      else {
+        $node = Node::create(
+          [
+            'type' => 'static_page',
+            'title' => "SocialistWorker raw front page from $year-$month-$day",
+            'field_static_body' => [
+              'value' => $raw_html,
+            ],
+            'status' => 1,
+            'uid' => $this->requestUID,
+            'path' => $url_alias,
+          ]
+        );
+        $node->save();
+      }
       // @todo Modify the body based on the actual NID + path alias.
       // @todo Harvest + save CSS+JS?
       if ($verbose) {
@@ -93,7 +112,12 @@ class DraftToLive {
           '@nid' => $node->id(),
           ':archive_url' => $archive_url->toString(),
         ];
-        drupal_set_message(t('Created a <a href=":archive_url">static page</a> (nid: @nid) to archive the current front page.', $placeholders));
+        if (!empty($alias)) {
+          drupal_set_message(t('Updated the existing <a href=":archive_url">static page</a> (nid: @nid) to archive the current front page.', $placeholders));
+        }
+        else {
+          drupal_set_message(t('Created a <a href=":archive_url">static page</a> (nid: @nid) to archive the current front page.', $placeholders));
+        }
       }
     }
     catch (RequestException $e) {
