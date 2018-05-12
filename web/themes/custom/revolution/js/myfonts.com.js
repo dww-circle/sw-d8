0f8998f92acf8f7033ebb96d5860dccff6cfe2a9
Zakiya <zakiya@chapterthree.com>
2018-05-11 21:05:13 -0700

    [SW #1084]: Removing references to Din Extra Light since we aren't using it..

--- web/themes/custom/revolution/js/myfonts.com.js
+++ web/themes/custom/revolution/js/myfonts.com.js
@@ -3,7 +3,7 @@
   var trial = document.createElement('script');
   trial.type = 'text/javascript';
   trial.async = true;
-  trial.src = 'https://easy.myfonts.net/v2/js?sid=161376(font-family=FF+DIN+Pro+Medium)&sid=161379(font-family=FF+DIN+Pro+Regular)&sid=161380(font-family=FF+DIN+Pro+Bold)&sid=290199(font-family=FF+DIN+Pro+Extra+Light)&key=SlmOcVxePJ';
+  trial.src = 'https://easy.myfonts.net/v2/js?sid=161376(font-family=FF+DIN+Pro+Medium)&sid=161379(font-family=FF+DIN+Pro+Regular)&sid=161380(font-family=FF+DIN+Pro+Bold)&key=SlmOcVxePJ';
   var head = document.getElementsByTagName("head")[0];
   head.appendChild(trial);
 })();
