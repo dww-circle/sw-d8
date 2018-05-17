888a267b956b3799ae67dfa8d36a315915558260
Zakiya <zakiya@chapterthree.com>
2018-05-17 15:19:39 -0700

    [SW #1084]: Using real purchased webfonts instead of myfonts.com trial script.

--- web/themes/custom/revolution/js/myfonts.com.js
+++
@@ -1,9 +0,0 @@
-// Intializes font. See https://www.myfonts.com/fonts/fontfont/ff-din/webfont_preview.html Good for 30 days.
-(function() {
-  var trial = document.createElement('script');
-  trial.type = 'text/javascript';
-  trial.async = true;
-  trial.src = 'https://easy.myfonts.net/v2/js?sid=161376(font-family=FF+DIN+Pro+Medium)&sid=161379(font-family=FF+DIN+Pro+Regular)&sid=161380(font-family=FF+DIN+Pro+Bold)&key=SlmOcVxePJ';
-  var head = document.getElementsByTagName("head")[0];
-  head.appendChild(trial);
-})();
