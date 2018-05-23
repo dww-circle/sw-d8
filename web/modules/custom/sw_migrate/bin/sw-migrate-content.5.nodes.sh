af87c2620bdaf4eac9ede9d5171048be6cf81f7c
Derek Wright <git@dwwright.net>
2018-05-23 08:34:32 -0700

    [SW #1131]: Restore migration for edition nodes (partly reverts commit e47000d2578d).

--- web/modules/custom/sw_migrate/bin/sw-migrate-content.5.nodes.sh
+++ web/modules/custom/sw_migrate/bin/sw-migrate-content.5.nodes.sh
@@ -1,13 +1,7 @@
 #!/bin/zsh
 
 # Actual nodes
-
-# 'story' still needs all sorts of help:
-# - Convert DME and move tags
-# - Set main image field
-# - rewrite hyphens to <hr>
-# ?
-for bundle (d6_node_page d6_node_person d6_node_insert_box d6_node_blog_couldnt_make_it_up d6_node_blog_critical_reading d6_node_story) {
+for bundle (d6_node_page d6_node_person d6_node_insert_box d6_node_blog_couldnt_make_it_up d6_node_blog_critical_reading d6_node_edition d6_node_story) {
   echo "Working on $bundle";
   rm -f $bundle.out
   /usr/bin/time drush mim upgrade_$bundle > $bundle.out 2>&1;
