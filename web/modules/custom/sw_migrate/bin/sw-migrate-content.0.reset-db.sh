740005c0a77e2ba1987ec788cf27d85175ec6fa7
Derek Wright <git@dwwright.net>
2018-05-25 13:57:45 -0700

    Migration: Final filenames for full migration.

--- web/modules/custom/sw_migrate/bin/sw-migrate-content.0.reset-db.sh
+++ web/modules/custom/sw_migrate/bin/sw-migrate-content.0.reset-db.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 drush -y sql-drop
-gzcat /Applications/MAMP/htdocs/sw-d8-dumps/2018-04-04.migrate-clean-start.sql.gz | `drush sql-connect`
+gzcat /Applications/MAMP/htdocs/sw-d8-dumps/sw-d8_dev_2018-05-25T14-32-16_UTC_database.sql.gz | `drush sql-connect`
 drush -y updb
 drush -y cim
 drush -y en sw_migrate
