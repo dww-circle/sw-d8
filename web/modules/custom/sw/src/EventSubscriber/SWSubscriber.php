f080a56dcfcef76b3682754fd822d388098e55b5
Derek Wright <git@dwwright.net>
2018-05-29 12:18:51 -0700

    [SW #1013]: Automatically redirect */feed to /recent/rss.xml.
    There are a lot of 404 errors in the logs for paths like
    /taxonomy/term/16685/0/feed so we should send those requests
    to our only 3.0 feed.

--- web/modules/custom/sw/src/EventSubscriber/SWSubscriber.php
+++ web/modules/custom/sw/src/EventSubscriber/SWSubscriber.php
@@ -52,7 +52,7 @@ class SWSubscriber implements EventSubscriberInterface {
   }
 
   /**
-   * Manipulates the request object.
+   * Handles all of SW's custom redirect logic.
    *
    * @param \Symfony\Component\HttpKernel\Event\GetResponseEvent $event
    *   The Event to process.
@@ -70,8 +70,13 @@ class SWSubscriber implements EventSubscriberInterface {
     elseif (preg_match('#^' . $base_path . '(\d+)$#', $request_uri, $matches)) {
       $response = new RedirectResponse($base_path . 'node/' . $matches[1], 301);
     }
+    // Redirect respond/blog/* to the /contact/readers_views form.
     elseif (preg_match('#respond/blog/(critical-reading|couldnt-make-it-up)#', $request_uri, $matches)) {
       $response = new RedirectResponse($base_path . 'contact/readers_views', 301);
+    }
+    // Redirect */feed to /recent/rss.xml.
+    elseif (preg_match('#^.*/feed$#', $request_uri, $matches)) {
+      $response = new RedirectResponse($base_path . 'recent/rss.xml', 301);
     }
     if (isset($response)) {
       $event->setResponse($response);
